15d62a2610218bae1381bf9bb7a2b37e8bc45115
Duong Minh Hoang <=>
2019-03-16 21:50:26 +0700

    Handle model for banners (MVP)

--- app/src/main/java/com/sun/music61/data/source/TracksDataSource.java
+++ app/src/main/java/com/sun/music61/data/source/TracksDataSource.java
@@ -1,4 +1,9 @@
 package com.sun.music61.data.source;
 
+import com.sun.music61.data.model.Track;
+
+import java.util.List;
+
 public interface TracksDataSource {
+    List<Track> getBanners();
 }
--- app/src/main/java/com/sun/music61/util/CommonUtils.java
+++ app/src/main/java/com/sun/music61/util/CommonUtils.java
@@ -1,5 +1,17 @@
 package com.sun.music61.util;
 
 public class CommonUtils {
-    public static final String URL_SERVER = "http://api.soundcloud.com/";
+
+    public static final String URL_SERVER = "http://api.soundcloud.com";
+
+    public interface APIReference {
+        String TRACKS = "/tracks";
+    }
+
+    public interface KeyParams {
+        String CLIENT_ID = "client_id=";
+        String TAGS = "tags=";
+        String LIMIT = "limit=";
+        String OFFSET = "offset=";
+    }
 }
