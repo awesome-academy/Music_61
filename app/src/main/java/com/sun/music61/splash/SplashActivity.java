cdc6fd0aefc5c03dd0a9222a9f8983e49c978817
Duong Minh Hoang <=>
2019-03-16 12:11:25 +0700

    Design UI HomeActivity

--- app/src/main/java/com/sun/music61/splash/SplashActivity.java
+++ app/src/main/java/com/sun/music61/splash/SplashActivity.java
@@ -22,7 +22,7 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.splash_act);
+        setContentView(R.layout.splash_activity);
         autoSwitchHomeActivity();
     }
 
