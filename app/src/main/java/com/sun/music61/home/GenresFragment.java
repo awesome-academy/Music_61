a208fb4ac129531f02cd7795dc2824a405da7f94
Duong Minh Hoang <=>
2019-03-17 15:22:45 +0700

    Set up ViewPager, Toolbar & Menu

---
+++ app/src/main/java/com/sun/music61/home/GenresFragment.java
@@ -0,0 +1,41 @@
+package com.sun.music61.home;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.sun.music61.R;
+import java.util.Objects;
+
+public class GenresFragment extends Fragment {
+
+    private static final String ARGUMENT_GENRES = "ARGUMENT_GENRES";
+
+    private String mGenres;
+
+    public static GenresFragment newInstance(String genres) {
+        GenresFragment fragment = new GenresFragment();
+        Bundle args = new Bundle();
+        args.putString(ARGUMENT_GENRES, genres);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public GenresFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        // Fetch Arguments
+        mGenres = Objects.requireNonNull(getArguments()).getString(ARGUMENT_GENRES);
+        View root = inflater.inflate(R.layout.genres_fragment, container, false);
+        TextView textGenres = root.findViewById(R.id.textGenres);
+        textGenres.setText(mGenres);
+        return root;
+    }
+}
